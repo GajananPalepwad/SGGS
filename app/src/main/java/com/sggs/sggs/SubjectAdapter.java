920b96916cd1d1af92ac8f3ec96bef85c6cfcb06
GN4k <2021bcs041@gmail.com>
2023-06-30 18:23:11 +0530

    Able to choose course

--- app/src/main/java/com/sggs/sggs/SubjectAdapter.java
+++ app/src/main/java/com/sggs/sggs/SubjectAdapter.java
@@ -1,40 +1,37 @@
 package com.sggs.sggs;
 
 import android.content.Context;
-import android.content.res.ColorStateList;
-import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
-
-import org.checkerframework.checker.units.qual.C;
 
 import java.util.ArrayList;
 import java.util.Random;
 
 public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.ViewHolder> {
 
-    private ArrayList<Subject> subjectList;
-    private Context context;
+    private static ArrayList<Subject> subjectList;
+    private static Context context;
 
     public SubjectAdapter(ArrayList<Subject> subjectList, Context context) {
         this.subjectList = subjectList;
         this.context = context;
-
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         ProgressBar progressBar;
         TextView subject;
         TextView marks;
-        CardView card ;
+        CardView card;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -42,8 +39,37 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.ViewHold
             subject = itemView.findViewById(R.id.subject);
             marks = itemView.findViewById(R.id.marks);
             card = itemView.findViewById(R.id.cardColor);
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            // Handle the click action here
+            int position = getAdapterPosition();
+            if (position != RecyclerView.NO_POSITION) {
+                // Perform the desired action for the clicked item
+                Subject subject = subjectList.get(position);
+                showConfirmationDialog(subject.getSubjectName());
+            }
+        }
+
+        private void showConfirmationDialog(String subject) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle(subject);
+
+            builder.setPositiveButton("Ok", (dialog, which) -> {
+                // Call the logout function
+                dialog.dismiss();
+            });
+
+
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+
     }
+
+
 
     @NonNull
     @Override
@@ -73,14 +99,13 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.ViewHold
 
         holder.subject.setText(abbreviation);
 
-        holder.marks.setText(String.valueOf(subject.getMarks()+"%"));
+        holder.marks.setText(String.valueOf(subject.getMarks() + "%"));
         holder.progressBar.setProgress(subject.getMarks());
         holder.progressBar.setMax(100);
 
-        switch (randomNumber){
+        switch (randomNumber) {
             case 1:
                 holder.card.setCardBackgroundColor(ContextCompat.getColor(context, R.color.color1));
-
                 break;
             case 2:
                 holder.card.setCardBackgroundColor(ContextCompat.getColor(context, R.color.color2));
@@ -95,7 +120,6 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.ViewHold
                 holder.card.setCardBackgroundColor(ContextCompat.getColor(context, R.color.color5));
                 break;
         }
-
     }
 
     @Override
@@ -103,3 +127,4 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.ViewHold
         return subjectList.size();
     }
 }
+
